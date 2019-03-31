b23c25f532d95c1e9c83746bb169a615176c6458
Liza-Kozlovskaya <liza.kozlovskaya.2011@mail.ru>
2019-04-01 00:44:40 +0300

    Edit lab12-13

--- lab12-13.ru/www/lab12.php
+++ lab12-13.ru/www/lab12.php
@@ -5,31 +5,95 @@
     <link href="add.css" rel="stylesheet">
 </head>
 <body>
+<form action="lab12.php" class="transparent" autocomplete="on" method=POST> 
+    <p>
+        <b>Введите директорию:</b>
+        <input id="path" name="dir" type="directory" autofocus required><br>
+    </p>
+    <p><input class="button" name="delete" type="submit" value="Удалить из всех файлов вещественные числа"/> </p>
+    <p>
+        <b>Добавить файл в директорию:</b><br>
+        <input type="name new file" name="new_file_1" >
+        <input type="text" placeholder=".txt" name="new_file_2" size="2">
+        <input class="button" name="add_file" type="submit" value="Добавить файл"/>
+    </p>
+    <textarea name="new file" cols=45 rows=10 ></textarea>
+    <p>
+        <b>Удалить файл из директории:</b><br>
+        <input type="name new file" name="delete_file_1" >
+        <input type="text" placeholder=".txt" name="delete_file_2" size="2">
+        <input class="button" name="delete_file" type="submit" value="Удалить файл"/>
+    </p>
+    <p>
+        <b>Выберите файл для удаление вещественных чисел:</b><br>
+        <input type="delete number" name="delete_number" >
+        <input class="button" name="delete_number" type="submit" value="Удалить числа"/>
+
+    </p>
+</form>
     <?php
-    /*Удалить из файла все вещественные числа из промежутка от 0.001 до 1.001. */
-    $dir = $_POST['dir'];
- 
+    function DeleteNumberInAllFile()
+    {
+        $dir = $_POST['dir'];
         foreach (glob("$dir/*.txt") as $filename) 
         {
             echo "<h3> $filename </h3>";
-                                            //file читаем содержимое файла в массив
+                            //file читаем содержимое файла в массив
             $data = file($filename, FILE_IGNORE_NEW_LINES); //FILE_IGNORE_NEW_LINES- отбрасываем перевод на новую строку
             //print_r($data);
             echo "<p> Данные в файле: </p>";
             $text = file_get_contents($filename);
             echo nl2br(htmlspecialchars($text));
-
+                                
             $patt = '~^(?:0\.(?:\d{1,2})?[1-9]|0\.(?:\d[1-9]|[1-9]\d)0|1(?:\.0{2}[01])?)$~';
             $data = array_filter(preg_replace($patt, '', $data));
             //print_r($data);
             echo "<p> Данные в файле после удаления вещественных чисел: </p>";
             file_put_contents($filename, join(PHP_EOL, $data)); //join объединяет элементы массива в строку
-                                                //PHP_EOL признак конца строки ИЛИ " "
-                                                //file_put_contents записывает данные в файл
+                                                                //PHP_EOL признак конца строки ИЛИ " "
+                                                                //file_put_contents записывает данные в файл
             $text2 = file_get_contents($filename);
             echo nl2br(htmlspecialchars($text2));
             echo "<br>";
         }
+    }
+
+    function AddFile()
+    {
+        $new_file= $_POST['new_file_1'].$_POST['new_file_2'];
+        echo $new_file;
+    }
+
+    function DeleteFile()
+    {
+        $delete_file = $_POST['delete_file_1'].$_POST['delete_file_2'];
+        echo $delete_file;
+    }
+
+    function DeleteNumber()
+    {
+        echo "Ok!";
+    }
+
+    if($_POST['delete'])
+    {
+        DeleteNumberInAllFile();
+    }
+
+    if($_POST['add_file'])
+    {
+        AddFile();
+    }
+
+    if($_POST['delete_file'])
+    {
+        DeleteFile();
+    }
+    if($_POST['delete_number'])
+    {
+        DeleteNumber();
+    }
+
     ?>
 </body>
 </html>
